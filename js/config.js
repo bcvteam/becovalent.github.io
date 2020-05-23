f1d4a7db4dba834cd67b7e0c88f26f05f39433f8
przemol <pstempor@gmail.com>
2020-05-23 16:47:37 +0100

    user registration and login

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_N3e39DRVZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1kvfeb67cv8rt0op2ie5jts3hk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
