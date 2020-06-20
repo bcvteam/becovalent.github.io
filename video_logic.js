4addcda384ebcffe45cc7ca97a3c53c5114777ac
przemol <pstempor@gmail.com>
2020-06-20 18:02:16 +0100

    play vidoe logic

--- video_logic.js
+++ video_logic.js
@@ -1,9 +1,19 @@
 
 $(function() {
-$('#unmute').change(function() {
+  $('#unmute').change(function() {
 
-  $("video").prop('muted', !$(this).prop('checked') );
-});
+    $("video").prop('muted', !$(this).prop('checked') );
+  });
+
+  $('#play').change(function() {
+
+    $("video")[0].requestFullscreen();
+  });
+
+
+
+
+
 });
 
 $(function () {
